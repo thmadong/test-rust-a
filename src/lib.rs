3eb29fd8e1d15799f45a78ad58e179491b1a772b
thmadong <2194667913@qq.com>
2020-04-29 11:08:07 +0800

    add module utils

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub mod utils {
+
+	pub struct Logger { }
+
+	impl Logger {
+		pub fn debug(s: &str) {
+	        println!("[test-rust-a:debug]: {}", s);
+	    }
+
+	    pub fn error(s: &str) {
+	        println!("[test-rust-a:error]: {}", s);
+	    }
+	}
+
+}
+
+#[cfg(test)]
+mod tests;
